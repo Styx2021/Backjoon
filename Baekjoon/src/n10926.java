d417a8ad39c51f525845e821368f844784292c66
Styx2021 <leeys6759@gmail.com>
2022-04-27 10:54:39 +0900

    레퍼지토리 생성

---
+++ Baekjoon/src/n10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class n10926 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.print(name+"??!");
+
+	}
+
+}
