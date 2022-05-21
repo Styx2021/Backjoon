1f40334b827e048d9fb86bf14528a9889f176c1d
Styx2021 <leeys6759@gmail.com>
2022-05-21 14:24:05 +0900

    1269 문제 해결

--- Backjoon/src/n1269.java
+++ Backjoon/src/n1269.java
@@ -1,48 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class n1269 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		TreeSet<Integer> set = new TreeSet<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		int A[] = new int[n];
-		int B[] = new int[m];
 
 		st = new StringTokenizer(br.readLine());
-
 		for (int i = 0; i < n; ++i) {
-			A[i] = Integer.parseInt(st.nextToken());
+			int element = Integer.parseInt(st.nextToken());
+			if (!set.add(element))
+				set.remove(element);
 		}
 
 		st = new StringTokenizer(br.readLine());
-
 		for (int i = 0; i < m; ++i) {
-			B[i] = Integer.parseInt(st.nextToken());
+			int element = Integer.parseInt(st.nextToken());
+			if (!set.add(element))
+				set.remove(element);
 		}
 
-		int cnt = 0;
-
-		for (int j = 0; j < m; ++j) {
-			if (Arrays.toString(A).contains(Integer.toString(B[j]))) {
-				cnt++;
-			}
-		}
-
-		for (int j = 0; j < n; ++j) {
-			if (Arrays.toString(B).contains(Integer.toString(A[j]))) {
-				cnt++;
-			}
-		}
-
-		System.out.println(cnt);
+		System.out.println(set.size());
 	}
 
 }
