d5268a0b7ffde7ee1cbe11ad40f13b14ba49bf33
Styx2021 <leeys6759@gmail.com>
2022-05-31 14:09:16 +0900

    15650 문제 해결

---
+++ Backjoon/src/n15650.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n15650 {
+
+	public static void dfs(int depth, int idx) {
+
+		if (depth == M) {
+			for (int i : list) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = idx; i < N; ++i) {
+
+			if (visit[i] == false) {
+				visit[i] = true;
+
+				list[depth] = i + 1;
+				dfs(depth + 1, i);
+
+				visit[i] = false;
+			}
+		}
+		return;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visit = new boolean[N];
+		list = new int[M];
+
+		for (int i = 0; i < N; ++i) {
+			visit[i] = false;
+		}
+
+		dfs(0, 0);
+	}
+
+	public static int N;
+	public static int M;
+	public static boolean visit[];
+	public static int list[];
+}
