53da37ed4abde7a0fe5f9be2f5450ecc20b30fee
Styx2021 <leeys6759@gmail.com>
2022-06-10 17:31:21 +0900

    2440 문제 해결

---
+++ Backjoon/src/n2440.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n2440 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; ++i) {
+			for (int j = i; j < N; ++j) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
