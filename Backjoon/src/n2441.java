59de2ceb662de7b58463c40e997f605ab9c18f17
Styx2021 <leeys6759@gmail.com>
2022-06-15 21:16:30 +0900

    2441 문제 해결

---
+++ Backjoon/src/n2441.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n2441 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; ++i) {
+			for (int k = N - i; k < N; ++k) {
+				System.out.print(" ");
+			}
+			for (int j = i; j < N; ++j) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
