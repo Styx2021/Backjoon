d242774030b8b4ae0dfb828745b8c7a5a2b37205
Styx2021 <leeys6759@gmail.com>
2022-06-20 17:58:48 +0900

    2490 문제 해결

---
+++ Backjoon/src/n2490.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n2490 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st;
+
+		for (int i = 0; i++ < 3;) {
+			st = new StringTokenizer(br.readLine());
+			int SUM = 0;
+			for (int j = 0; j++ < 4;) {
+				SUM += Integer.parseInt(st.nextToken());
+			}
+			switch (SUM) {
+			case 4:
+				System.out.println("E");
+				break;
+			case 3:
+				System.out.println("A");
+				break;
+			case 2:
+				System.out.println("B");
+				break;
+			case 1:
+				System.out.println("C");
+				break;
+			case 0:
+				System.out.println("D");
+				break;
+			}
+		}
+
+	}
+
+}
