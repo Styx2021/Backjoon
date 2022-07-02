5a9430577c63de95131b40e10b5f005a1ade3aa8
Styx2021 <leeys6759@gmail.com>
2022-07-02 20:51:30 +0900

    UCPC_A 문제 해결

---
+++ Backjoon/src/UCPC_A.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UCPC_A {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N / 4; i++) {
+			sb.append("long ");
+		}
+
+		sb.append("int");
+
+		System.out.println(sb.toString());
+	}
+
+}
