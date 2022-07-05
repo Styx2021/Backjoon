73be41bbe4117dc7ca9bf16e3245a517523830f6
Styx2021 <leeys6759@gmail.com>
2022-07-05 20:10:40 +0900

    1550 문제 해결

---
+++ Backjoon/src/n1550.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n1550 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int result = Integer.parseInt(br.readLine(), 16);
+
+		System.out.println(result);
+	}
+
+}
